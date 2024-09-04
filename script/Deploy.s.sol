c21b3e874a1533f48dcc54e6f2d1f33922710438
katzman <steve.katzman@coinbase.com>
2024-09-04 13:32:40 -0700

    Get script ready for deployment

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -6,7 +6,10 @@ import {console} from "forge-std/console.sol";
 import {Basefriends} from "src/Basefriends.sol";
 
 contract Deploy is Script {
+
+    uint256 pkey = vm.envUint("PRIVATE_KEY");
     function run() public {
+        vm.broadcast(pkey);
         address registry = 0x1493b2567056c2181630115660963E13A8E32735; //https://sepolia.basescan.org/address/0x1493b2567056c2181630115660963E13A8E32735
 
         Basefriends bf = new Basefriends(registry);
